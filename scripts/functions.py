2516980998e1f50fd1b81b9bac02378110367e1c
Lane <lane.clark.web@gmail.com>
2020-11-20 16:29:58 -0800

    Created functions.py.

---
+++ scripts/functions.py
@@ -0,0 +1,69 @@
+######################
+# Variables
+######################
+
+covid_list = ['covid','virus', 'corona','ncov','sars','pandemic', 'new case', 'active case', 'social distanc', 'mask', 'ppe', 'quarantine']
+
+rt_regex = "^rt"
+
+######################
+# Preprocessing Functions
+######################
+
+def preprocess(text, hashtags=False, join=False):
+    """Strips out URLs, usernames, punctuation, and other unwanted text.
+    Tweets are tokenzied and stop words removed. Removing hashtags and
+    joining tokens back into a string is optional.
+    
+    Can create a new column with optional arguments via:
+    
+    df['new_col'] = df['col'].apply(lambda x: preprocess(x, hashtags=True))
+    """
+    text = text.lower()
+    if hashtags:
+        text = ' '.join(re.sub(r"\#\w*[a-zA-Z]+\w*","",text).split())
+    text = ' '.join(re.sub("((www\.[\S]+)|(https?://[\S]+))","",text).split())
+    text = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)","",text).split())
+    text = ' '.join(re.sub("^\n","",text).split())
+    text = ' '.join(re.sub("^rt","",text).split())
+    punc = ''.join([char for char in text if char not in string.punctuation])
+    tokens = word_tokenize(punc)
+    stops = [word for word in tokens if word not in stop_words]
+    if join:
+        stops = (' ').join(stops)
+    return stops
+
+def joiner(text):
+    """Simple function to join a list together into one string"""
+    string = (' ').join(text)
+    return string
+
+######################
+# Feature Functions
+######################
+
+def covid_mention(text, synonyms=covid_list):
+    """ Checks tweet for presence of any word from the
+    """
+    for term in synonyms:
+        if term in text:
+            return 1
+        continue
+    return 0
+
+# Create binary column for is_retweet
+def is_retweet(text):
+    if re.match(rt_regex, text) is not None:
+        return 1
+    return 0
+
+
+######################
+
+######################
+
+
+
+######################
+
+######################
