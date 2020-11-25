794c8c7362608941dc0dccc8b2205cfdc01d2397
Lane <lane.clark.web@gmail.com>
2020-11-25 12:36:07 -0800

    preprocess function now contains all text processing steps, with option to leave out some stages

--- scripts/functions.py
+++ scripts/functions.py
@@ -63,23 +63,34 @@ def col_filter(df,cols=['created_at','user','full_text','retweet_count', 'retwee
     df = df[cols]
     return df
 
-def preprocess(text, hashtags=False, join=False):
+def preprocess(text, hashtags=False, join=False, url=False, user=True, emo=False):
     """
     Strips out URLs, usernames, punctuation, and other unwanted text.
-    Tweets are tokenzied and stop words removed. Removing hashtags and
-    joining tokens back into a string is optional.
+    Tweets are tokenzied and stop words removed. Removing hashtags, URLs, 
+    user mentions and joining tokens back into a string is optional.
     
     Example - Creating a new column in DataFrame:
     
-    df['new_col'] = df['full_text'].apply(lambda x: preprocess(x, hashtags=True))
+    df['new_col'] = df['full_text'].apply(lambda x: preprocess(x, url=True, join=True, emo=True))
     """
     text = text.lower()
     if hashtags:
         text = ' '.join(re.sub(r'\#\w*[a-zA-Z]+\w*','',text).split())
+    if url:
+        text = ' '.join(re.sub("((www\.[\S]+)|(https?://[\S]+))","URL",text).split())
     text = ' '.join(re.sub('((www\.[\S]+)|(https?://[\S]+))','',text).split())
-    text = ' '.join(re.sub('(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)','',text).split())
+    if user:
+        text = ' '.join(re.sub("(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)"," USER ",text).split())
+    if emo:
+        # Positive Emoji - Smile, Laugh, Wink,Love
+        text = ' '.join(re.sub('(:\s?\)|:-\)|:-\)\)|;\)|\(\s?:|\(-:|:\’\))',' emopos ',text).split()) 
+        text = ' '.join(re.sub('(:\s?D|:-D|x-?D|X-?D)',' emopos ',text).split()) 
+        text = ' '.join(re.sub('(<3|:\*)',' emopos ',text).split()) 
+        # Negative Emoji - Sad, Cry
+        text = ' '.join(re.sub('(:\s?\(|:-\(|:\||\)\s?:|\)-:)',' emoneg ',text).split())
+        text = ' '.join(re.sub('(:,\(|:\’\(|:"\()',' emoneg ',text).split())
     text = ' '.join(re.sub('^\n','',text).split())
-    text = ' '.join(re.sub('^&amp','',text).split()) # Added on Nov 24th 4:44PM
+    text = ' '.join(re.sub('amp;',' ',text).split()) # Added on Nov 24th 4:44PM
     text = ' '.join(re.sub('^rt','',text).split())
     punc = ''.join([char for char in text if char not in string.punctuation])
     tokens = word_tokenize(punc)
